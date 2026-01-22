ddcf344ac0a38d7b995f63b84648e623132aee2b
Xristos <xrsapostolidis@gmail.com>
2026-01-22 19:00:28 +0200

    add MainButton helper component

---
+++ src/components/MainButton.tsx
@@ -0,0 +1,31 @@
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  className?: string;
+  disabled?: boolean;
+}
+
+const MainButton: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  type = "button",
+  className = "",
+  disabled = false,
+}) => {
+  const baseStyles =
+    "cursor-pointer mr-8 border-2 border-border-soft rounded-xl px-4 py-2 hover:bg-primary hover:text-black transition disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:opacity-50 cursor-pointer font-semibold py-2 px-6 rounded-lg shadow-md";
+
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      className={`${baseStyles} ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default MainButton;
